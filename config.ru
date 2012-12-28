ca549687e803e1f36547fa5431bcba5c1d41b813
jhjguxin <864248765@qq.com>
2012-12-28 18:29:02 +0800

    wrap main code in Server class

--- config.ru
+++ config.ru
@@ -7,20 +7,32 @@ require File.join(File.dirname(__FILE__),"server")
 #use Rack::CommonLogger, logger
 
 set :raise_errors, true
-set :views, File.dirname(__FILE__) + '/views'
-set :public, File.dirname(__FILE__) + '/public'
-set :app_file, __FILE__
 
-log = File.new("./log/#{Sinatra::Base.environment}.log", "a")
-STDOUT.reopen(log)
-STDERR.reopen(log)
-$stdout.sync=true
-$stderr.sync=true
 
+class Server
+  configure :production do
+    set :haml, { :ugly=>true }
+    set :clean_trace, true
+    Dir.mkdir('log') unless File.exist?('log')
+
+    $logger = Logger.new("./log/#{Sinatra::Base.environment}.log",'weekly')
+    $logger.level = Logger::WARN
+
+    # Spit stdout and stderr to a file during production
+    # in case something goes wrong
+    $stdout.reopen("./log/#{Sinatra::Base.environment}_stdout.log", "w")
+    $stdout.sync = true
+    $stderr.reopen($stdout)
+  end
+
+  configure :development do
+    $logger = Logger.new(STDOUT)
+  end
+end
 
 set :sessions, true
 set :logging, true
 set :dump_errors, false
 
-run Sinatra::Application
+run Server
 
--- server.rb
+++ server.rb
@@ -27,47 +27,50 @@ DataMapper.auto_upgrade!
 WeiXin::Config.token = "weixin"
 WeiXin::Config.url = "http://weixin.bbtang.com"
 
-#token, timestamp, nonce
-def geneate_signature(token,timestamp,nonce)
-  signature = [token.to_s,timestamp.to_s,nonce.to_s].sort.join("")
-  Digest::SHA1.hexdigest(signature)
-end
-
-def valid_signature?(signature,timestamp,nonce)
-  token = WeiXin::Config.token
-  if signature.present? and token.present? and timestamp.present? and nonce.present?
-    guess_signature = geneate_signature(token,timestamp,nonce)
-    guess_signature.eql? signature
+class Server < Sinatra::Application
+  #token, timestamp, nonce
+  def geneate_signature(token,timestamp,nonce)
+    signature = [token.to_s,timestamp.to_s,nonce.to_s].sort.join("")
+    Digest::SHA1.hexdigest(signature)
   end
-end
 
-#http://www.ruby-doc.org/stdlib-1.9.3/libdoc/rexml/rdoc/REXML.html
-#http://www.germane-software.com/software/rexml/docs/tutorial.html
-#==========xml generate
+  def valid_signature?(signature,timestamp,nonce)
+    token = WeiXin::Config.token
 
-#
+    if signature.present? and token.present? and timestamp.present? and nonce.present?
+      guess_signature = geneate_signature(token,timestamp,nonce)
+      guess_signature.eql? signature
+    end
+  end
+
+  #http://www.ruby-doc.org/stdlib-1.9.3/libdoc/rexml/rdoc/REXML.html
+  #http://www.germane-software.com/software/rexml/docs/tutorial.html
+  #==========xml generate
+
+  #
 =begin
 * signature — 微信加密签名
 * timestamp — 时间戳
 * nonce — 随机数
 * echostr — 随机字符串
 =end
-#params
-def present?(val)
-  !val.nil? and !val.empty?
-end
-
-#curl http://localhost:4567/?nonce=121212121&signature=f3739ef63eaeaafc6e935ab9202f6e0e4bee2c03&timestamp=1356601689&echostr=22222222222222222
-get '/' do
-  #token= params[:token]
-  if valid_signature?(signature= params[:signature], timestamp = params[:timestamp], nonce= params[:nonce] )
-    logger.info("signature is ok and return #{params[:echostr]}")
-    puts "signature is ok and return #{params[:echostr]}"
-    params[:echostr]
+  #params
+  def present?(val)
+    !val.nil? and !val.empty?
   end
-end
 
-post '/' do
-  #puts Hash.from_xml params
-  puts request.body
+  #curl http://localhost:4567/?nonce=121212121&signature=f3739ef63eaeaafc6e935ab9202f6e0e4bee2c03&timestamp=1356601689&echostr=22222222222222222
+  get '/' do
+    #token= params[:token]
+    if valid_signature?(signature= params[:signature], timestamp = params[:timestamp], nonce= params[:nonce] )
+      logger.info("signature is ok and return #{params[:echostr]}")
+      puts "signature is ok and return #{params[:echostr]}"
+      params[:echostr]
+    end
+  end
+
+  post '/' do
+    #puts Hash.from_xml params
+    puts request.body
+  end
 end
